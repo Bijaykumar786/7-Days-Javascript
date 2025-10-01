bb39ff82db06c15c32491953c0c90e144c55264c
bijaykumar786 <vijay786das@gmail.com>
2025-10-01 14:47:47 +0545

    first commit

---
+++ 2days/variables.js
@@ -0,0 +1,24 @@
+
+var bottle = "water"  // string 
+
+let bag =  "books"  // string
+
+const pi = 3.14  // number
+
+var isRaining = true  // boolean
+
+let age = 25  // number
+
+console.log(bottle)
+console.log(bag)
+console.log(pi)
+console.log(isRaining)
+console.log(age)
+
+console.log("mero bootle ma k xa ?" + bottle)
+console.log("mero bag ma k xa ?" + bag )
+console.log(`pi ko vlaue ${pi} hun xa`)
+
+console.log(`${isRaining} aaj pani parxa r`)
+
+
